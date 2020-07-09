3f1e572cb0425a2ce1409957f6598cc0b28a4ed9
juntagrico-valposchiavo <64611762+juntagrico-valposchiavo@users.noreply.github.com>
2020-07-09 11:36:47 +0200

    Update settings.py
    Try to use the business regulations instead of bylaws

--- ravisintera/settings.py
+++ ravisintera/settings.py
@@ -169,7 +169,7 @@ SHARE_PRICE = "50"
 DEFAULT_MAILER = 'ravisintera.mailer.Mailer'
 
 ## Ravis in tera configuration (will, 9th July 2020):
-#BYLAWS = "https://449f11ee-d3df-49ec-b517-64c66e1cafa7.filesusr.com/ugd/5d5472_17e1fef49ee445fba5783b14daf81143.pdf"
+BUSINESS_REGULATIONS = "https://449f11ee-d3df-49ec-b517-64c66e1cafa7.filesusr.com/ugd/5d5472_17e1fef49ee445fba5783b14daf81143.pdf"
 BYLAWS = "https://www.ravisintera.com/fam-tosio-raselli"
 ENABLE_SHARES = False
 BASE_FEE = "50"
